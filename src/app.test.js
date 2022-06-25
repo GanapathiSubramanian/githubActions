61c773b5de1cf6f6d105b561d600aaad92440513
Ganapathi Subramanian V <gbalaji411@gmail.com>
2022-06-25 12:43:26 +0530

    update

--- src/app.test.js
+++ src/app.test.js
@@ -2,10 +2,14 @@ const app = require("./app")
 const supertest = require("supertest")
 const request = supertest(app)
 
+const Todos = require('./index');
+const assert = require('assert').strict;
+
 describe("/test endpoint", () => {
     it("should return a response", async () => {
         const response = await request.get("/test")
-        expect(response.status).toBe(200)
-        expect(response.text).toBe("Hello world");
+        // /expect(response.status).toBe(200)
+        // expect(response.text).toBe("Hello world");
+        assert.equal(response.status,201)
     })
 })
